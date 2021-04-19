63e97f13d3c7583aa3931e0e31e3f9e8dd133f75
Rajnandiinii <75487390+Rajnandiinii@users.noreply.github.com>
2021-04-19 19:59:53 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,28 @@
+class Paper{
+    constructor(x, y, width,height) {
+        var options = {
+            isStatic:false,
+            'restitution':0.3,
+            'friction':0.5,
+            'density':2.0
+        }
+        this.body = Bodies.rectangle(x, y, width,height, options);
+       this.width=width;
+       this.height=height;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        fill("brown");
+        ellipeMode(RADIUS) 
+        ellipse(0,0,this.width.this.height);
+        pop();
+      
+       
+        
+       
+      }
+}
---
+++ sketch.js
@@ -0,0 +1,44 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1400, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+paper=new Paper(150,300,30,30);
+ground=new Ground(700,550,1400,20);
+r1=new Dustbin(1000,540,200,20);
+r2=new Dustbin(900,500,20,200);
+r3=new Dustbin(1100,540,200,20);
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  paper.display();
+  ground.display();
+  r1.display();
+  r2.display();
+  r3.display();
+  drawSprites();
+ 
+}
+
+
+
+
